695325a6fca570761a1a31ec6e1f33708ad35905
Haruma Akimoto <siba16fb@gmail.com>
2020-12-23 15:46:51 +0900

    pinAssignment.hの追加

--- mechanumWheelRobot.ino
+++ mechanumWheelRobot.ino
@@ -8,27 +8,11 @@
  * CopyRight www.osoyoo.com
 */
 #include <Servo.h>
-#define speedPinR 9   //  Front Wheel PWM pin connect Right MODEL-X ENA 
-#define RightMotorDirPin1  22    //Front Right Motor direction pin 1 to Right MODEL-X IN1  (K1)
-#define RightMotorDirPin2  24   //Front Right Motor direction pin 2 to Right MODEL-X IN2   (K1)                                 
-#define LeftMotorDirPin1  26    //Front Left Motor direction pin 1 to Right MODEL-X IN3 (K3)
-#define LeftMotorDirPin2  28   //Front Left Motor direction pin 2 to Right MODEL-X IN4 (K3)
-#define speedPinL 10   //  Front Wheel PWM pin connect Right MODEL-X ENB
 
-#define speedPinRB 11   //  Rear Wheel PWM pin connect Left MODEL-X ENA 
-#define RightMotorDirPin1B  5    //Rear Right Motor direction pin 1 to Left  MODEL-X IN1 ( K1)
-#define RightMotorDirPin2B 6    //Rear Right Motor direction pin 2 to Left  MODEL-X IN2 ( K1) 
-#define LeftMotorDirPin1B 7    //Rear Left Motor direction pin 1 to Left  MODEL-X IN3  (K3)
-#define LeftMotorDirPin2B 8  //Rear Left Motor direction pin 2 to Left  MODEL-X IN4 (K3)
-#define speedPinLB 12    //  Rear Wheel PWM pin connect Left MODEL-X ENB
-
+#include "pinAssignment.h"
 
 #define LPT 2 // scan loop coumter
 
-#define SERVO_PIN     13  //servo connect to D5
-
-#define Echo_PIN    31 // Ultrasonic Echo pin connect to A5
-#define Trig_PIN    30  // Ultrasonic Trig pin connect to A4
 
 #define FAST_SPEED  160   //both sides of the motor speed
 #define SPEED  120     //both sides of the motor speed
