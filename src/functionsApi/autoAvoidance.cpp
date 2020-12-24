78a9284958390458794d909b679c914f1f9106f8
Haruma Akimoto <siba16fb@gmail.com>
2020-12-24 18:24:40 +0900

    autoAvoidanceリファクタリング

--- src/functionsApi/autoAvoidance.cpp
+++ src/functionsApi/autoAvoidance.cpp
@@ -31,54 +31,75 @@ void autoAvoidance(){
   ++numcycles;
   if(numcycles>=LPT){ //Watch if something is around every LPT loops while moving forward 
     moveStop();
-    String obstacle_sign=watchSurrounding(); // 5 digits of obstacle_sign binary value means the 5 direction obstacle status
-      Serial.print("begin str=");
-        Serial.println(obstacle_sign);
-                    if( obstacle_sign=="10000"){
-     Serial.println("SLIT right");
-          moveSetSpeed(FAST_SPEED,SPEED,FAST_SPEED,SPEED);
-     moveAdvance();
- 
+    String obstacle_sign = watchSurrounding(); // 5 digits of obstacle_sign binary value means the 5 direction obstacle status
+    
+    DebugLogPrintln("begin str="+obstacle_sign);
+    if( obstacle_sign=="10000" ){
+      DebugLogPrintln("SLIT Right");
+      moveSetSpeed(FAST_SPEED,SPEED,FAST_SPEED,SPEED);
+      moveAdvance();
       delay(turntime);
       moveStop();
-    }
-        else    if( obstacle_sign=="00001"  ){
-      Serial.println("SLIT LEFT");
+    }else
+    if( obstacle_sign=="00001" ){
+      DebugLogPrintln("SLIT Left");
       moveSetSpeed(SPEED,FAST_SPEED,SPEED,FAST_SPEED);
       moveAdvance();
   
       delay(turntime);
       moveStop();
-    }
-    else if( obstacle_sign=="11100" || obstacle_sign=="01000" || obstacle_sign=="11000"  || obstacle_sign=="10100"  || obstacle_sign=="01100" ||obstacle_sign=="00100"  ||obstacle_sign=="01000" ){
-      Serial.println("hand right");
+    }else 
+    if( obstacle_sign=="11100" || 
+        obstacle_sign=="01000" || 
+        obstacle_sign=="11000" || 
+        obstacle_sign=="10100" || 
+        obstacle_sign=="01100" ||
+        obstacle_sign=="00100" ||
+        obstacle_sign=="01000" )
+    {
+      DebugLogPrintln("hand right");
 	  moveRight();
       moveSetSpeed(TURN_SPEED,TURN_SPEED,TURN_SPEED,TURN_SPEED);
       delay(turntime);
       moveStop();
-    } 
-    else if( obstacle_sign=="00010" || obstacle_sign=="00111" || obstacle_sign=="00011"  || obstacle_sign=="00101" || obstacle_sign=="00110" || obstacle_sign=="01010" ){
-    Serial.println("hand left");
+    }else 
+    if( obstacle_sign=="00010" || 
+        obstacle_sign=="00111" || 
+        obstacle_sign=="00011" || 
+        obstacle_sign=="00101" || 
+        obstacle_sign=="00110" || 
+        obstacle_sign=="01010" )
+    {
+      DebugLogPrintln("hand left");
       moveLeft();//Turn left
       moveSetSpeed(TURN_SPEED,TURN_SPEED,TURN_SPEED,TURN_SPEED);
       delay(turntime);
       moveStop();
-    }
- 
-    else if(  obstacle_sign=="01111" ||  obstacle_sign=="10111" || obstacle_sign=="11111"  ){
-    Serial.println("hand back left");
+    }else 
+    if(  obstacle_sign=="01111" ||
+         obstacle_sign=="10111" ||
+         obstacle_sign=="11111"  )
+    {
+      DebugLogPrintln("hand back left");
 	  moveBack();
 	  moveSetSpeed( BACK_SPEED1,BACK_SPEED2,BACK_SPEED1,BACK_SPEED2);
       delay(backtime);
       moveStop();
-    } 
-    else if( obstacle_sign=="11011"  ||    obstacle_sign=="11101"  ||  obstacle_sign=="11110"  || obstacle_sign=="01110"  ){
-    Serial.println("hand back right");
+    }else 
+    if( obstacle_sign=="11011"  ||
+        obstacle_sign=="11101"  ||
+        obstacle_sign=="11110"  || 
+        obstacle_sign=="01110"  )
+    {
+      DebugLogPrintln("hand back right");
       moveBack();
       moveSetSpeed(BACK_SPEED2,BACK_SPEED1,BACK_SPEED2,BACK_SPEED1);
       delay(backtime);
       moveStop();
-    }else Serial.println("no handle");
+    }else{
+      DebugLogPrintln("no handle");
+    }
+
     numcycles=0; //Restart count of cycles
   } else {
       moveSetSpeed(SPEED,SPEED,SPEED,SPEED);
@@ -91,13 +112,17 @@ void autoAvoidance(){
   
   distance = watch(); // use the watch() function to see if anything is ahead (when the robot is just moving forward and not looking around it will test the distance in front)
   if (distance<distancelimit){ // The robot will just stop if it is completely sure there's an obstacle ahead (must test 25 times) (needed to ignore ultrasonic sensor's false signals)
- Serial.println("final go back");
+    DebugLogPrintln("final go back");
 	moveBack();
-  moveSetSpeed(BACK_SPEED1,BACK_SPEED2,BACK_SPEED1,BACK_SPEED2);
-  delay(backtime);
-      ++thereis;}
+    moveSetSpeed(BACK_SPEED1,BACK_SPEED2,BACK_SPEED1,BACK_SPEED2);
+    delay(backtime);
+    ++thereis;
+  }
+  
   if (distance>distancelimit){
-      thereis=0;} //Count is restarted
+      thereis=0;
+  } //Count is restarted
+
   if (thereis > 25){
   Serial.println("final stop");
     moveStop(); // Since something is ahead, stop moving.
