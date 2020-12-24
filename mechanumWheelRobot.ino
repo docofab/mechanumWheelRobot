5c2f96e39f7caa9331568cee1c1567fc5e542ede
Haruma Akimoto <siba16fb@gmail.com>
2020-12-24 13:07:38 +0900

    メインを変更に追従

--- mechanumWheelRobot.ino
+++ mechanumWheelRobot.ino
@@ -7,176 +7,15 @@
  * Tutorial URL http://osoyoo.com/?p=30022
  * CopyRight www.osoyoo.com
 */
-#include <Servo.h>
 
 #include "src/lib/pinAssignment.h"
 #include "src/lib/debugPrint.h"
 
 #include "src/hardWareDriver/motorDriver.h"
+#include "src/hardWareDriver/servoDriver.h"
+#include "src/hardWareDriver/ultraSonicSenser.h"
 
-#define LPT 2 // scan loop coumter
-
-#define FAST_SPEED  160   //both sides of the motor speed
-#define SPEED  120     //both sides of the motor speed
-#define TURN_SPEED  120   //both sides of the motor speed
-#define BACK_SPEED1  160   //back speed
-#define BACK_SPEED2  90    //back speed
-
-int leftscanval, centerscanval, rightscanval, ldiagonalscanval, rdiagonalscanval;
-const int distancelimit = 30; //distance limit for obstacles in front           
-const int sidedistancelimit = 30; //minimum distance in cm to obstacles at both sides (the car will allow a shorter distance sideways)
-int distance;
-int numcycles = 0;
-const int turntime = 250; //Time the robot spends turning (miliseconds)
-const int backtime = 300; //Time the robot spends turning (miliseconds)
-
-int thereis;
-Servo head;
-
-
-//Meassures distances to the right, left, front, left diagonal, right diagonal and asign them in cm to the variables rightscanval, 
-//leftscanval, centerscanval, ldiagonalscanval and rdiagonalscanval (there are 5 points for distance testing)
-String watchsurrounding(){
-/*  obstacle_status is a binary integer, its last 5 digits stands for if there is any obstacles in 5 directions,
- *   for example B101000 last 5 digits is 01000, which stands for Left front has obstacle, B100111 means front, right front and right ha
- */
- 
-int obstacle_status =B100000;
-  centerscanval = watch();
-  if(centerscanval<distancelimit){
-    stop_Stop();
-    
-    obstacle_status  =obstacle_status | B100;
-    }
-  head.write(120);
-  delay(100);
-  ldiagonalscanval = watch();
-  if(ldiagonalscanval<distancelimit){
-    stop_Stop();
-    
-     obstacle_status  =obstacle_status | B1000;
-    }
-  head.write(170); //Didn't use 180 degrees because my servo is not able to take this angle
-  delay(300);
-  leftscanval = watch();
-  if(leftscanval<sidedistancelimit){
-    stop_Stop();
-    
-     obstacle_status  =obstacle_status | B10000;
-    }
-
-  head.write(90); //use 90 degrees if you are moving your servo through the whole 180 degrees
-  delay(100);
-  centerscanval = watch();
-  if(centerscanval<distancelimit){
-    stop_Stop();
-    
-    obstacle_status  =obstacle_status | B100;
-    }
-  head.write(40);
-  delay(100);
-  rdiagonalscanval = watch();
-  if(rdiagonalscanval<distancelimit){
-    stop_Stop();
-    
-    obstacle_status  =obstacle_status | B10;
-    }
-  head.write(0);
-  delay(100);
-  rightscanval = watch();
-  if(rightscanval<sidedistancelimit){
-    stop_Stop();
-    
-    obstacle_status  =obstacle_status | 1;
-    }
-  head.write(90); //Finish looking around (look forward again)
-  delay(300);
-   String obstacle_str= String(obstacle_status,BIN);
-  obstacle_str= obstacle_str.substring(1,6);
-  
-  return obstacle_str; //return 5-character string standing for 5 direction obstacle status
-}
-
-void auto_avoidance(){
-
-  ++numcycles;
-  if(numcycles>=LPT){ //Watch if something is around every LPT loops while moving forward 
-     stop_Stop();
-    String obstacle_sign=watchsurrounding(); // 5 digits of obstacle_sign binary value means the 5 direction obstacle status
-      Serial.print("begin str=");
-        Serial.println(obstacle_sign);
-                    if( obstacle_sign=="10000"){
-     Serial.println("SLIT right");
-          set_Motorspeed(FAST_SPEED,SPEED,FAST_SPEED,SPEED);
-     go_Advance();
- 
-      delay(turntime);
-      stop_Stop();
-    }
-        else    if( obstacle_sign=="00001"  ){
-     Serial.println("SLIT LEFT");
-       set_Motorspeed(SPEED,FAST_SPEED,SPEED,FAST_SPEED);
-      go_Advance();
-  
-      delay(turntime);
-      stop_Stop();
-    }
-    else if( obstacle_sign=="11100" || obstacle_sign=="01000" || obstacle_sign=="11000"  || obstacle_sign=="10100"  || obstacle_sign=="01100" ||obstacle_sign=="00100"  ||obstacle_sign=="01000" ){
-     Serial.println("hand right");
-	    go_Right();
-      set_Motorspeed(TURN_SPEED,TURN_SPEED,TURN_SPEED,TURN_SPEED);
-      delay(turntime);
-      stop_Stop();
-    } 
-    else if( obstacle_sign=="00010" || obstacle_sign=="00111" || obstacle_sign=="00011"  || obstacle_sign=="00101" || obstacle_sign=="00110" || obstacle_sign=="01010" ){
-    Serial.println("hand left");
-     go_Left();//Turn left
-     set_Motorspeed(TURN_SPEED,TURN_SPEED,TURN_SPEED,TURN_SPEED);
-      delay(turntime);
-      stop_Stop();
-    }
- 
-    else if(  obstacle_sign=="01111" ||  obstacle_sign=="10111" || obstacle_sign=="11111"  ){
-    Serial.println("hand back left");
-	  go_Back();
-		set_Motorspeed( BACK_SPEED1,BACK_SPEED2,BACK_SPEED1,BACK_SPEED2);
-       delay(backtime);
-          stop_Stop();
-        } 
-         else if( obstacle_sign=="11011"  ||    obstacle_sign=="11101"  ||  obstacle_sign=="11110"  || obstacle_sign=="01110"  ){
-    Serial.println("hand back right");
-   go_Back();
-    set_Motorspeed(BACK_SPEED2,BACK_SPEED1,BACK_SPEED2,BACK_SPEED1);
-       delay(backtime);
-          stop_Stop();
-        }    
-  
-        else Serial.println("no handle");
-    numcycles=0; //Restart count of cycles
-  } else {
-     set_Motorspeed(SPEED,SPEED,SPEED,SPEED);
-     go_Advance();  // if nothing is wrong go forward using go() function above.
-        delay(backtime);
-          stop_Stop();
-  }
-  
-  //else  Serial.println(numcycles);
-  
-  distance = watch(); // use the watch() function to see if anything is ahead (when the robot is just moving forward and not looking around it will test the distance in front)
-  if (distance<distancelimit){ // The robot will just stop if it is completely sure there's an obstacle ahead (must test 25 times) (needed to ignore ultrasonic sensor's false signals)
- Serial.println("final go back");
-	go_Back();
-  set_Motorspeed(BACK_SPEED1,BACK_SPEED2,BACK_SPEED1,BACK_SPEED2);
-  delay(backtime);
-      ++thereis;}
-  if (distance>distancelimit){
-      thereis=0;} //Count is restarted
-  if (thereis > 25){
-  Serial.println("final stop");
-    stop_Stop(); // Since something is ahead, stop moving.
-    thereis=0;
-  }
-}
+#include ""
 
 void setup() {
 
@@ -184,21 +23,12 @@ void setup() {
   motorInitialize();
   stop_Stop();//stop move
 
+  /*init servo*/
+  servoInit();
+
   /*init HC-SR04*/
   ultraSonicSensorInitialize();
 
-  /*init buzzer*/
-  digitalWrite(Trig_PIN,LOW);
-
-  /*init servo*/
-  head.attach(SERVO_PIN); 
-  head.write(0);
-  delay(1000);
-  head.write(170);
-  delay(1000);
-  head.write(90);
-  delay(2000);
-  
   Serial.begin(9600);
  
   stop_Stop();//Stop
